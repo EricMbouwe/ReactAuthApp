b9c536003d794ff8d5902b229914f48ef9769606
ericmbouwe <ericmbouwe@yahoo.fr>
2020-07-05 14:19:26 +0100

    added dashboard and home component

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  state = {  }
+  render() { 
+    return (
+      <div>
+        <h1>Home</h1>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
